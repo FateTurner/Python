b1494caecec8bacb200f28fc73d8038a698c4d7e
Fate-Turner <fateturn@gmail.com>
2019-05-22 21:46:54 +0530

    added one line

--- demo.py
+++ demo.py
@@ -1,2 +1,8 @@
-print("Hello universe")
-print("Welcome to Fate turner")
+given_list = [7, 5, 4, 4, 3, 1, -2, -3, -5, -7]
+total = 0
+
+for i in given_list:
+    if i < 0:
+        total = i + total
+print(total)
+
