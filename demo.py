4d25130e09b52d471981ce7464fcd7d5a3afbb57
Fate-Turner <fateturn@gmail.com>
2019-05-22 22:45:26 +0530

    added one line

--- demo.py
+++ demo.py
@@ -6,3 +6,13 @@ for i in given_list:
         total = i + total
 print(total)
 
+
+
+
+total = 0
+print(list(range(100)))
+
+for i in range(1, 100):
+    if i % 3 == 0 or i % 5 == 0:
+        total = total + i
+print(total)
