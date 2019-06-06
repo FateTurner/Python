e4871616499660b5276831e871fbf36625efe02c
Fate-Turner <fateturn@gmail.com>
2019-06-06 16:58:22 +0530

    Tic Tac toe

--- demo.py
+++ demo.py
@@ -1,59 +1,200 @@
-given_list = [7, 5, 4, 4, 3, 1, -2, -3, -5, -7]
-total = 0
 
-for i in given_list:
-    if i < 0:
-        total = i + total
-print(total)
+game = [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]
 
 
+#print(game)
 
-total = 0
-print(list(range(100)))
-
-for i in range(1, 100):
-    if i % 3 == 0 or i % 5 == 0:
-        total = total + i
-print(total)
+def gameboard():
+    for row in game:
+        print(row)
 
 
 
-def string_multiplication(s, ni):
-    print(s * ni)
+def plotting():
+    for row in game:
+        print(row)
+    print("Please type the position you want to place on")
 
-string_multiplication( "Hi", 2)
+plotting()
+
+input1 = int(input())
+
+#Row one if else
+if input1 == 1:
+    game[0][0] = 'X'
+elif input1 == 2:
+    game[0][1] = 'X'
+elif input1 == 3:
+     game[0][2] = 'X'
+
+#row two
+elif input1 == 4:
+    game[1][0] = 'X'
+elif input1 == 5:
+    game[1][1] = 'X'
+elif input1 == 6:
+     game[1][2] = 'X'
+#row three
+elif input1 == 7:
+    game[2][0] = 'X'
+elif input1 == 8:
+    game[2][1] = 'X'
+elif input1 == 9:
+     game[2][2] = 'X'
+
+gameboard()
+
+input2 = int(input())
+print("Please type the position the second player wants to play on")
+
+if input2 == 1:
+    game[0][0] = 'Y'
+elif input2 == 2:
+    game[0][1] = 'Y'
+elif input2 == 3:
+     game[0][2] = 'Y'
+
+
+elif input2 == 4:
+    game[1][0] = 'Y'
+elif input2 == 5:
+    game[1][1] = 'Y'
+elif input2 == 6:
+     game[1][2] = 'Y'
+
+elif input2 == 7:
+    game[2][0] = 'Y'
+elif input2 == 8:
+    game[2][1] = 'Y'
+elif input2 == 9:
+     game[2][2] = 'Y'
+
+gameboard()
+
+input1 = int(input())
+
+print("Please type the position the first player wants to play on")
+if input1 == 1:
+    game[0][0] = 'X'
+elif input1 == 2:
+    game[0][1] = 'X'
+elif input1 == 3:
+     game[0][2] = 'X'
+
+
+elif input1 == 4:
+    game[1][0] = 'X'
+elif input1 == 5:
+    game[1][1] = 'X'
+elif input1 == 6:
+     game[1][2] = 'X'
+
+elif input1 == 7:
+    game[2][0] = 'X'
+elif input1 == 8:
+    game[2][1] = 'X'
+elif input1 == 9:
+     game[2][2] = 'X'
 
 
 
-def Greet(name):
-    print("Hey! Wassup?" + name)
 
-Greet("Bob")
+gameboard()
 
+input2 = int(input())
+print("Please type the position the second player wants to play on")
 
-a = [2, 3, 9]
-max_len = len(a)
-
-for i in a:
-    if a[0] == 6:
-        print("True")
-    elif a[max_len - 1] == 6:
-        print("True")
-    else:
-        print("False")
-    break
+if input2 == 1:
+    game[0][0] = 'Y'
+elif input2 == 2:
+    game[0][1] = 'Y'
+elif input2 == 3:
+     game[0][2] = 'Y'
 
 
+elif input2 == 4:
+    game[1][0] = 'Y'
+elif input2 == 5:
+    game[1][1] = 'Y'
+elif input2 == 6:
+     game[1][2] = 'Y'
 
-count_evens = [2, 1, 2, 3, 4]
-
-count = 0
-for x in count_evens:
-    if x % 2 == 0:
-        count = count + 1
-    else:
-        x = x + 1
-print(count)
+elif input2 == 7:
+    game[2][0] = 'Y'
+elif input2 == 8:
+    game[2][1] = 'Y'
+elif input2 == 9:
+     game[2][2] = 'Y'
 
 
 
+gameboard()
+
+input1 = int(input())
+
+print("Please type the position the first player wants to play on")
+if input1 == 1:
+    game[0][0] = 'X'
+elif input1 == 2:
+    game[0][1] = 'X'
+elif input1 == 3:
+     game[0][2] = 'X'
+
+
+elif input1 == 4:
+    game[1][0] = 'X'
+elif input1 == 5:
+    game[1][1] = 'X'
+elif input1 == 6:
+     game[1][2] = 'X'
+
+elif input1 == 7:
+    game[2][0] = 'X'
+elif input1 == 8:
+    game[2][1] = 'X'
+elif input1 == 9:
+     game[2][2] = 'X'
+
+
+
+
+gameboard()
+
+input2 = int(input())
+print("Please type the position the second player wants to play on")
+
+if input2 == 1:
+    game[0][0] = 'Y'
+elif input2 == 2:
+    game[0][1] = 'Y'
+elif input2 == 3:
+     game[0][2] = 'Y'
+
+
+elif input2 == 4:
+    game[1][0] = 'Y'
+elif input2 == 5:
+    game[1][1] = 'Y'
+elif input2 == 6:
+     game[1][2] = 'Y'
+
+elif input2 == 7:
+    game[2][0] = 'Y'
+elif input2 == 8:
+    game[2][1] = 'Y'
+elif input2 == 9:
+     game[2][2] = 'Y'
+
+
+
+gameboard()
+
+
+
+if game[0][0] == 'X' and game[1][1] == 'X' and game[2][2] == 'X':
+    print("Strike!!")
+
+elif game[0][0] == 'Y' and game[1][1] == 'Y' and game[2][2] == 'Y':
+    print("Strike ")
